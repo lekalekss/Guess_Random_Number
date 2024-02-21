6dcbe5c57f09e8caaa251cb0ce956d0a4f398e25
lekalekss <155861509+lekalekss@users.noreply.github.com>
2024-02-20 20:22:24 -0600

    new file

--- RandomNumberGuess/Program.cs
+++ RandomNumberGuess/Program.cs
@@ -1,15 +1,145 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography;
+using System.Text;
 
 class RandomNumberGenerator
-{
+{    
     static void Main()
     {
         Console.OutputEncoding = Encoding.UTF8;
         Console.InputEncoding = Encoding.Unicode;
+
+        Console.WriteLine("Welcome to the game 'Guess the number!'");
+        Console.WriteLine("I guessed the number between: 1 to 100");
+        Console.WriteLine("Try to guess");
+        Random guess = new Random();
+
+        int guessNumber = guess.Next(1, 100);
+                        
+        List<int> guessList = new List<int>();
+
+        while (true)
+        {            
+            string try1 = Console.ReadLine();
+            bool tryConsole = int.TryParse(try1 , out int number);
+
+            if (tryConsole == true)
+            {                
+                guessList.Add(number);
+                int countTimesToGuess = guessList.Count;
+                
+                if (guessNumber == number)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("Good job!!!");
+                    Console.WriteLine("You guessed this number by " + countTimesToGuess + " times");
+                    Console.ResetColor();
+
+                    break;
+                }
+
+                if (number != guessNumber)
+                {
+                    if (number < guessNumber && number >0)
+                    {
+                        Console.WriteLine("The hidden number is higher. \nNext chance: ");
+                        continue;
+                    }
+
+                    if (number > guessNumber && number < 100)
+                    {
+                        Console.WriteLine("The hidden number is lower. \nNext chance: ");
+                        continue;
+                    }
+                    if (number < 0 || number > 100)
+                    {
+                        Console.WriteLine("The hidden number is between 1 and 100");
+                        continue;
+                    }
+                }
+            }
+
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Please choose number from 1 to 100");
+                Console.ResetColor();
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //Random guess = new Random();
+
         
-        Console.WriteLine("Добро пожаловать в игру 'Угадай число'!");
-        Console.WriteLine("Я загадал число между: 1 и 100.");
-        Console.WriteLine("Попробуйте угадать.");
+        //    int guessNumber = guess.Next(1,100);
+        
+
+        //Console.WriteLine(guessNumber);
+        //while (true)
+        //{
+        //        string try1 = Console.ReadLine();
+        //        int tryNumber = int.Parse(try1);
+
+        //    int count = tryNumber.ToString().Count();
+        //    Console.WriteLine(count);
+
+            
+        
+        //    if (guessNumber == tryNumber)
+        //    {
+        //        Console.WriteLine("Good job");
+
+        //        break;
+        //    }
+
+        //    if (guessNumber != tryNumber) 
+        //    {
+        //        Console.WriteLine("Next chance");
+        //        continue;
+        //    }
+        //}
+        
+
+        
+        
+
+
+      
 
         // Создаем объект для генерации чисел
         
@@ -40,5 +170,7 @@ class RandomNumberGenerator
         // Если пользователь ввел не число, то выводим сообщение об ошибке и просим ввести число еще раз
         // Число джолжно быть в диапазоне от 1 до 100 и целое
         
-    }
-}
+    
+
+   
+
